4fca97e05b8532acee0a6aca2ee6d844e8a4949e
Abi Raskin <abiraskin@gmail.com>
2019-01-08 01:32:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
     students = []
-    index_page.css("div.roster-cards-cotainer").each do |card|
+    index_page.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
         student_profile = "#{student.attr("href")}"
         student_location = student.css(".student-location").text
@@ -37,5 +37,6 @@ class Scraper
 
     student
   end
+
 
 end
